94393a4bf8ec81152d79413c1b3c7bb3b7d0fa0f
mukhin-sn <senikomu@gmail.com>
2024-01-09 03:08:50 +0700

    Minor changes to the project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.hh_areas import *
 from test import get_employers, get_vacancies
-
+from src.hh_class import *
 
 def main():
     db_name = "hh_ru"
@@ -16,24 +16,24 @@ def main():
 
     # Тестирование функции get_vacancies()
     # ===============================================
-    txt = "Python"
-    emp_id = '49357'  # '1455'
-    area = 113
-    params = {
-        "per_page": 100,
-        "area": area,
-        # "text": f"NAME:{txt}",
-        "employer_id": emp_id,
-        "only_with_salary": True,
-        }
-
-    for i in range(20):
-        print("=" * 300)
-        params["page"] = i
-        vac = get_vacancies(params)
-        # print(vac[0])
-        for j in vac[1]:
-            print(j)
+    # txt = "Python"
+    # emp_id = '49357'  # '1455'
+    # area = 113
+    # params = {
+    #     "per_page": 100,
+    #     "area": area,
+    #     # "text": f"NAME:{txt}",
+    #     "employer_id": emp_id,
+    #     "only_with_salary": True,
+    #     }
+    #
+    # for i in range(20):
+    #     print("=" * 300)
+    #     params["page"] = i
+    #     vac = get_vacancies(params)
+    #     # print(vac[0])
+    #     for j in vac[1]:
+    #         print(j)
     # ===============================================
 
     # Тестирование функции get_areas()
@@ -57,7 +57,26 @@ def main():
     # ===============================================
 
 
-
 if __name__ == '__main__':
     main()
 
+    # Берём данные из сохраненного файла
+    data_list = HhClass.load_from_file('src/out_vac.json')
+    data_list = data_list['object']
+
+    # Определяем количество нужных записей
+    count = len(data_list)
+
+    # Формируем словарь и список из ID - работодателей и количества открытых ими вакансий
+    out_list = []
+    out_dict = {}
+    for val_lst in data_list:
+        for key, val in val_lst.items():
+            out_dict.update({key: len(val)})
+            out_list.append(dict(id=key, found_vacancy=len(val)))
+
+    for i in out_list:
+        print(i)
+
+    # Сохраняем полученный список в файл, для дальнейшей обработки
+    # HhClass.save_to_file('src/found_vacancy.json', out_list)
